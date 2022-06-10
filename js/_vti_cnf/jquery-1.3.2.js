8b656ba25413ac5b0a95239d605c393ca5689f0e
nlis28852178 <71814297+nlis28852178@users.noreply.github.com>
2022-06-10 09:01:45 +0800

    0000

---
+++ js/_vti_cnf/jquery-1.3.2.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Mar 2012 00:48:07 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|仁寶園地NEW3/imagespaceright.htm 仁寶園地/baby-slideshow.html 仁寶園地/marriage-slideshow.html 仁寶園地/images-slideshow.html 仁寶園地/cartoon-slideshow.html
+vti_author:SR|HACO-COM\\HACO
+vti_modifiedby:SR|HACO-COM\\HACO
+vti_timecreated:TR|10 Mar 2012 13:11:22 -0000
+vti_nexttolasttimemodified:TR|10 Mar 2012 13:11:22 -0000
+vti_cacheddtm:TX|24 Mar 2012 00:48:07 -0000
+vti_filesize:IR|120619
---
+++ js/_vti_cnf/jquery.opacityrollover.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Mar 2012 00:48:05 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|仁寶園地/baby-slideshow.html 仁寶園地/marriage-slideshow.html 仁寶園地/cartoon-slideshow.html 仁寶園地/images-slideshow.html
+vti_author:SR|HACO-COM\\HACO
+vti_modifiedby:SR|HACO-COM\\HACO
+vti_timecreated:TR|10 Mar 2012 13:12:13 -0000
+vti_nexttolasttimemodified:TR|10 Mar 2012 13:12:13 -0000
+vti_cacheddtm:TX|24 Mar 2012 00:48:05 -0000
+vti_filesize:IR|937
